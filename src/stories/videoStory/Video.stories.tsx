77044413df6ab53e3212c3ee0b2c434a29c26c54
syoo970 <syu9710@gmail.com>
2023-01-03 17:44:04 +0900

    feat: video.js 컴포넌트

---
+++ src/stories/videoStory/Video.stories.tsx
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+import Video from "@/components/Video";
+
+export default {
+  title: "video/player",
+  component: Video,
+  argTypes: {
+    src: {
+      control: "select",
+      options: ["starwars", "city", "beach"],
+    },
+  },
+  args: {
+    sources: [{ src: "/starwars.mp4", type: "video/mp4" }],
+    styles: {
+      width: "500px",
+      aspectRatio: "16 / 9",
+    },
+    videoOptions: {
+      controls: true,
+      autoplay: true,
+    },
+  },
+} as ComponentMeta<typeof Video>;
+
+const Template: ComponentStory<typeof Video> = (args) => {
+  const { sources } = args;
+  const [videoSource, setVideoSource] = useState(
+    sources ?? [{ src: "/starwars.mp4", type: "video/mp4" }]
+  );
+
+  const handleSelectChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const value = e.target.value;
+    setVideoSource([{ src: `/${value}.mp4`, type: "video/mp4" }]);
+  };
+
+  return (
+    <div>
+      <Video {...args} sources={videoSource} />
+      <select onChange={handleSelectChange}>
+        <option value="starwars">Starwars</option>
+        <option value="city">City</option>
+        <option value="beach">Beach</option>
+      </select>
+    </div>
+  );
+};
+
+export const Default = Template.bind({});
